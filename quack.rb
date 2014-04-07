6059d5008864b7de876b4659d6722e2b46873232
Ryan Moser <ryanpmoser@gmail.com>
2014-04-07 08:37:29 -0700

    [REFACTOR] break out methods for each http verb

--- quack.rb
+++ quack.rb
@@ -4,6 +4,7 @@ require 'pry-debugger'
 
 class TinyServer
   attr_reader :server
+  attr_accessor :socket, :request_line
 
   DEFAULT_PORT = 4444
 
@@ -13,36 +14,95 @@ class TinyServer
 
   def start
     loop do
-      socket = server.accept
-      request_line = socket.gets
+      self.socket = server.accept
+
+      self.request_line = socket.gets
 
       STDERR.puts request_line
 
-      path = requested_file(request_line)
-      path = File.join(path, 'index.html') if File.directory?(path)
-
-      if File.exist?(path) && !File.directory?(path)
-        File.open(path, 'rb') do |file|
-          socket.print "HTTP/1.1 200 OK\r\n" <<
-                       "Content-Type: #{content_type(file)}\r\n" <<
-                       "Content-Length: #{file.size}\r\n" <<
-                       "Connection: close\r\n\r\n"
-
-          IO.copy_stream(file, socket)
-        end
-      else
-        message = "File not found\n"
-
-        socket.print "HTTP/1.1 404 Not Found\r\n" <<
-                     "Content-Type: text/plain\r\n" <<
-                     "Content-Length: #{message.size}\r\n" <<
-                     "Connection: close\r\n\r\n"
-
-        socket.print message
-      end
+      public_send "do_#{requested_http_verb}"
 
       socket.close
     end
+  end
+
+  HTTP_VERBS = %w{GET HEAD PUT POST DELETE}
+
+  def do_GET(head=false)
+    path = requested_file(request_line)
+    path = File.join(path, 'index.html') if File.directory?(path)
+
+    if is_file?(path)
+      File.open(path, 'rb') do |file|
+        socket.print "HTTP/1.1 200 OK\r\n" <<
+                     "Content-Type: #{content_type(file)}\r\n" <<
+                     "Content-Length: #{file.size}\r\n" <<
+                     "Connection: close\r\n\r\n"
+
+        IO.copy_stream(file, socket) unless head
+      end
+    else
+      message = "File not found\n"
+
+      socket.print "HTTP/1.1 404 Not Found\r\n" <<
+                   "Content-Type: text/plain\r\n" <<
+                   "Content-Length: #{message.size}\r\n" <<
+                   "Connection: close\r\n\r\n"
+
+      socket.print message unless head
+    end
+  end
+
+  def do_HEAD
+    do_GET(head=true)
+  end
+
+  def do_DELETE
+    path = requested_file(request_line)
+    if is_file?(path)
+      File.delete(path)
+    end
+    message = "#{path} deleted"
+    socket.print response_header(204, message)
+    socket.print message
+  end
+
+  def do_PUT
+    message = "Not Implemented"
+    socket.print response_header(501, message)
+    socket.print message
+  end
+
+  def do_POST
+    message = "Not Implemented"
+    socket.print response_header(501, message)
+    socket.print message
+  end
+
+  private
+
+  STATUS_CODE_MAPPINGS = {
+    200 => "OK",
+    201 => "Created",
+    204 => "No Content",
+    404 => "Not Found",
+    500 => "Internal Server Error",
+    501 => "Not Implemented"
+  }
+
+  def response_header(status_code, message)
+    "HTTP/1.1 #{status_code} #{STATUS_CODE_MAPPINGS[status_code]}\r\n" <<
+    "Content-Type: text/plain\r\n" <<
+    "Content-Length: #{message.size}\r\n" <<
+    "Connection: close\r\n\r\n"
+  end
+
+  def is_file?(path)
+    File.exist?(path) && !File.directory?(path)
+  end
+
+  def requested_http_verb
+    HTTP_VERBS.find {|verb| request_line.start_with? verb}
   end
 
   CONTENT_TYPE_MAPPING = {
@@ -78,5 +138,6 @@ class TinyServer
     File.join(WEB_ROOT, *clean)
   end
 end
+
 
 TinyServer.new.start
